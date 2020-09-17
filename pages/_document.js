e63240319e5fffc1fab47b24bf28f25751d08adb
huy <truongduchuy910@gmail.com>
2020-09-17 09:07:04 +0700

    build

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="vi">
         <Head />
         <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
         <title>Shop Whey Đà Nẵng | Dinh Dưỡng Thể Thao</title>
