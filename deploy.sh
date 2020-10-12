de299557f668cebe32c39525148ba5440666aebe
huy <truongduchuy910@gmail.com>
2020-10-12 13:48:16 +0700

    build

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-echo "current branch ".$branch
-
-echo "export"
 npm run export
-
-echo "move"
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+mkdir ../ThousandTimes/$branch
 cp -r ./out/* ../ThousandTimes/$branch
 rm -fr ./out
-
-echo "push"
 git add .
 git commit -m "build"
 git push origin $branch 
-
-echo "deploy"
 cd ../ThousandTimes
 git add .
 git commit -m "build"
